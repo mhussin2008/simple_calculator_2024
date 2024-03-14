71cf5f63069fae190cc81886b6ab1b942bc6ff8b
MohamedHussin <mhussin2008@yahoo.com>
2024-03-14 15:36:15 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/nadanewsoft/simple_calculator_2024/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nadanewsoft.simple_calculator_2024
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'Calculator.dart';
+
+void main() => runApp(const CalculatorApp());
+
+class CalculatorApp extends StatelessWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Calculator(),
+    );
+  }
+}
